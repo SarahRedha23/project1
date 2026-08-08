263f06d5a4820d7bf4dbf37e7825c95a296607c6
SarahRedha23 <sarahredha23@icloud.com>
2026-08-08 14:57:42 +0300

    Generate Wordle game board

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let gameOver
 const boardEl = document.querySelector("#board")
 const keyboardEl = document.querySelector("#keyboard")
 const messageEl = document.querySelector("#message")
-const restartBin = document.querySelector("#restart-bin")
+const restartBin = document.querySelector("#restart-btn")
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -28,6 +28,19 @@ function init() {
     messageEl.textContent = "Good Luck!"
 }
 
+function createBoard(){
+    for(let row =0; row < ROWS; row++){
+            for(let col = 0; col < COLS; col++){
+            
+             const tile = document.createElement("div")
+             tile.classList.add("tile")
+             boardEl.appendChild(tile)    
+    }
+        
+    }
+}
+
 /*----------------------------- Event Listeners ------------------------------*/
 
 init()
+createBoard()
